6619a298cdaf59596157a207ebbadc62219eea99
Pratap <pratap.gwl@gmail.com>
2014-07-03 22:44:18 +0530

    commit cassandra client

---
+++ cassandra-client/src/main/java/com/practics/client/CassandraClient.java
@@ -0,0 +1,37 @@
+package com.practics.client;
+import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.Host;
+import com.datastax.driver.core.Metadata;
+
+public class CassandraClient {
+
+	private Cluster cluster;
+
+	public void connect(String node) {
+		cluster = Cluster.builder().addContactPoint(node)
+				// .withSSL() // Uncomment if using client to node encryption
+				.build();
+		Metadata metadata = cluster.getMetadata();
+		System.out.printf("Connected to cluster: %s\n",
+				metadata.getClusterName());
+		for (Host host : metadata.getAllHosts()) {
+			System.out.printf("Datacenter: %s; Host: %s; Rack: %s\n",
+					host.getDatacenter(), host.getAddress(), host.getRack());
+		}
+	}
+
+	public void close() {
+		cluster.shutdown();
+	}
+	
+	public static void main(String[] args){
+		
+		CassandraClient client = new CassandraClient();
+		
+		client.connect("127.0.0.1");
+		
+		//client.connect("localhost");
+		
+		client.close();
+	}
+}
